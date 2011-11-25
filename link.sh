6afdb45ccf3fe17ba9426cc95600846081195c33
pnaimoli <pnaimoli@422844d7-f33f-73c6-4c74-fcd1dd919437>
2011-11-25 19:05:05 +0000

    Add helpful script to quick link dotfiles

---
+++ link.sh
@@ -0,0 +1,8 @@
+ln -F -s .Rprofile ~/
+ln -F -s .bash_profile ~/
+ln -F -s .inputrc ~/
+ln -F -s .lesskey ~/
+ln -F -s .lesskey-in ~/
+ln -F -s .synergy.conf ~/
+ln -F -s .vimrc ~/
+ln -F -s .xmodmaprc ~/
