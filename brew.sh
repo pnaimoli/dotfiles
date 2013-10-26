5d83faa3195e47eff32361eb4d369c9c0428402e
pnaimoli@gmail.com <pnaimoli@gmail.com@422844d7-f33f-73c6-4c74-fcd1dd919437>
2013-10-26 22:48:52 +0000

    Holy wow! homebrew cask is awesome

---
+++ brew.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# install brew stuff
+brew install macvim
+brew install mosh
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# install casks
+brew cask install google-chrome
+brew cask install adium
+brew cask install size-up
+#brew cask install silverlight
+brew cask install dropbox
+brew cask install steam
+brew cask install vlc
+brew cask install transmission
+brew cask install seashore
+brew cask install rstudio
+brew cask install kindle
+brew cask install firefox
