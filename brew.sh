00c1d92d6b33ad59688c699f928e58d5788022c8
pnaimoli <peter@dhcp-10-2-200-34.hudson-trading.com>
2013-11-26 09:18:29 -0500

    uncomment witch installation now that cask is fixed.

--- brew.sh
+++ brew.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# make sure we're updated
+brew update
+brew tap phinze/homebrew-cask
+brew install brew-cask
+brew upgrade brew-cask
+
 # install brew stuff
 brew install macvim
 brew install mosh
@@ -11,32 +18,31 @@ brew install svn
 brew install cowsay
 brew install gs imagemagick
 
-brew tap phinze/homebrew-cask
-brew install brew-cask
-
 # install casks
-brew cask install google-chrome
 brew cask install adium
-brew cask install sizeup
-brew cask install silverlight
-brew cask install dropbox
-brew cask install steam
-brew cask install vlc
-brew cask install transmission
-brew cask install seashore
-brew cask install rstudio
-brew cask install kindle
-brew cask install firefox
-#brew cask install little-snitch
-brew cask install lastpass-universal
-brew cask install iterm2
-brew cask install ksdiff
-brew cask install github
-brew cask install multibit
-#brew cask install witch
-brew cask install x-quartz
 brew cask install day-o
+brew cask install dropbox
+brew cask install firefox
+brew cask install github
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install kindle
+brew cask install ksdiff
+brew cask install lastpass-universal
+brew cask install multibit
+brew cask install rstudio
+brew cask install seashore
+brew cask install silverlight
+brew cask install sizeup
+brew cask install steam
+brew cask install transmission
+brew cask install vlc
+brew cask install witch
+brew cask install xquartz
+#brew cask install little-snitch
 
 # must be after x-quartz
 brew tap homebrew/science
 brew install gfortran R
+
+#find ~/Applications -type l -name MacVim.app | while read f; do osascript -e "tell app \"Finder\" to make new alias file at POSIX file \"/Applications\" to POSIX file \"$(stat -f%Y "$f")\""; rm $f; done
