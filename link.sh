11d82a09796aef939780a15395ab11751ec8f1c4
Peter Naimoli <pnaimoli@gmail.com>
2014-04-11 09:15:43 -0400

    fix some stuff after removing dots

--- link.sh
+++ link.sh
@@ -1,16 +1,16 @@
-ln -f -s ~/.dotfiles/Rprofile ~/.Rprofile
-ln -f -s ~/.dotfiles/bash_profile ~/.bash_profile
-ln -f -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -f -s ~/.dotfiles/gvimrc ~/.gvimrc
-ln -f -s ~/.dotfiles/inputrc ~/.inputrc
-ln -f -s ~/.dotfiles/lesskey ~/.lesskey
-ln -f -s ~/.dotfiles/lesskey-in ~/.lesskey-in
-ln -f -s ~/.dotfiles/synergy.conf ~/.synergy.conf
-ln -f -s ~/.dotfiles/vimrc ~/.vimrc
-ln -f -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -f -s ~/.dotfiles/pydistutils.cfg ~/.pydistutils.cfg
+ln -snf ~/.dotfiles/Rprofile ~/.Rprofile
+ln -snf ~/.dotfiles/bash_profile ~/.bash_profile
+ln -snf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -snf ~/.dotfiles/gvimrc ~/.gvimrc
+ln -snf ~/.dotfiles/inputrc ~/.inputrc
+ln -snf ~/.dotfiles/lesskey ~/.lesskey
+ln -snf ~/.dotfiles/lesskey-in ~/.lesskey-in
+ln -snf ~/.dotfiles/synergy.conf ~/.synergy.conf
+ln -snf ~/.dotfiles/vimrc ~/.vimrc
+ln -snf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -snf ~/.dotfiles/pydistutils.cfg ~/.pydistutils.cfg
 
-ln -f -s ~/.dotfiles/vim ~/.vim
-ln -f -s ~/.dotfiles/config ~/.config
+ln -snf ~/.dotfiles/vim ~/.vim
+ln -snf ~/.dotfiles/config ~/.config
 
 ln -s ~/.dotfiles/bashrc_mac ~/.bashrc
