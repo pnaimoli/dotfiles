531c0487bffe5af9d5270fb3b33af7c0038340b9
Peter Naimoli <peter@hudson-trading.com>
2014-04-11 09:09:57 -0400

    rename a bunch of stuff to get rid of dots

--- link.sh
+++ link.sh
@@ -1,14 +1,16 @@
-ln -f -s ~/.dotfiles/.Rprofile ~/
-ln -f -s ~/.dotfiles/.bash_profile ~/
-ln -f -s ~/.dotfiles/.gitconfig ~/
-ln -f -s ~/.dotfiles/.gvimrc ~/
-ln -f -s ~/.dotfiles/.inputrc ~/
-ln -f -s ~/.dotfiles/.lesskey ~/
-ln -f -s ~/.dotfiles/.lesskey-in ~/
-ln -f -s ~/.dotfiles/.synergy.conf ~/
-ln -f -s ~/.dotfiles/.vim ~/
-ln -f -s ~/.dotfiles/.vimrc ~/
-ln -f -s ~/.dotfiles/.tmux.conf ~/
-ln -f -s ~/.dotfiles/.pydistutils.cfg ~/
+ln -f -s ~/.dotfiles/Rprofile ~/.Rprofile
+ln -f -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -f -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -f -s ~/.dotfiles/gvimrc ~/.gvimrc
+ln -f -s ~/.dotfiles/inputrc ~/.inputrc
+ln -f -s ~/.dotfiles/lesskey ~/.lesskey
+ln -f -s ~/.dotfiles/lesskey-in ~/.lesskey-in
+ln -f -s ~/.dotfiles/synergy.conf ~/.synergy.conf
+ln -f -s ~/.dotfiles/vimrc ~/.vimrc
+ln -f -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -f -s ~/.dotfiles/pydistutils.cfg ~/.pydistutils.cfg
+
+ln -f -s ~/.dotfiles/vim ~/.vim
 ln -f -s ~/.dotfiles/config ~/.config
-ln -s ~/.dotfiles/.bashrc_mac ~/.bashrc
+
+ln -s ~/.dotfiles/bashrc_mac ~/.bashrc
