173bc803798795142a2b69fad6bb0c0d641c1bb9
Peter Naimoli <pnaimoli@users.noreply.github.com>
2017-11-04 13:08:00 -0500

    Removed pydistutils.cfg.
    That problem is no longer relevant.

--- link.sh
+++ link.sh
@@ -2,7 +2,6 @@ ln -snf ~/.dotfiles/Rprofile ~/.Rprofile
 ln -snf ~/.dotfiles/gvimrc ~/.gvimrc
 ln -snf ~/.dotfiles/vimrc ~/.vimrc
 ln -snf ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -snf ~/.dotfiles/pydistutils.cfg ~/.pydistutils.cfg
 
 ln -snf ~/.dotfiles/vim ~/.vim
 ln -snf ~/.dotfiles/config ~/.config
